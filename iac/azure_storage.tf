0d68c3895250f9986b6e68b1b3a6367ac274de3f
Facundo Gauna <facundo.gauna@newsignature.com>
2020-03-23 21:11:53 -0400

    adding a storage account with static file hosting

---
+++ iac/azure_storage.tf
@@ -0,0 +1,13 @@
+resource "azurerm_storage_account" "storage_account" {
+  name                = local.storage_account_name
+  resource_group_name = azurerm_resource_group.resource_group.name
+
+  location                 = var.location
+  account_tier             = "Standard"
+  account_replication_type = "LRS"
+  account_name             = "StorageV2"
+
+  static_website {
+
+  }
+}
--- iac/main.tf
+++ iac/main.tf
@@ -9,10 +9,12 @@ terraform {
 }
 
 locals {
+  app_name_alphanumeric = replace(replace(var.app_name, "-", ""), "_", "")
   resource_group_name = "rg-${var.app_name}-${var.environment}"
   application_insights_name = "ai-${var.app_name}-${var.environment}"
   app_service_name = "azapp-${var.app_name}-${var.environment}"
   app_service_plan_name = "azappsp-${var.app_name}-${var.environment}"
+  storage_account_name = "st${local.app_name_alphanumeric}"
 }
 
 resource "azurerm_resource_group" "resource_group" {
