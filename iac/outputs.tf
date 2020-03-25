1fa063f38f9a6deb0f55246d5e9c8658d915da50
Chris Ayers <clayers@gmail.com>
2020-03-25 08:18:38 -0400

    Added app insights to export

--- iac/outputs.tf
+++ iac/outputs.tf
@@ -5,3 +5,7 @@ output "app_service" {
 output "storage_account" {
     value = azurerm_storage_account.storage_account.name
 }
+
+output "app_insights_instrumentation_key" {
+    value = azurerm_application_insights.insights.instrumentation_key    
+}
