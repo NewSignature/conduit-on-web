6aeca2f5a38909cf188359f224d7adb9faf2d039
John Callaway <john.callaway@newsignature.com>
2020-04-03 16:28:59 -0400

    Added identity type of SystemAssigned to app_service

--- iac/app_service.tf
+++ iac/app_service.tf
@@ -4,6 +4,10 @@ resource "azurerm_app_service" "app_service" {
   resource_group_name = azurerm_resource_group.resource_group.name
   app_service_plan_id = azurerm_app_service_plan.app_plan.id
 
+  identity {
+    type = "SystemAssigned"
+  }
+
   site_config {
     dotnet_framework_version = "v4.0"
   }
